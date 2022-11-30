ab696ea60ccd7f465c02b598b38fb2dbe1edfbd6
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-11-30 10:17:38 +0100

    controlers actualizados

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -19,4 +19,22 @@ class PageController extends AbstractController
     {
         return $this->render('page/about.html.twig', []);
     }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
+
+    #[Route('/services', name: 'services')]
+    public function services(): Response
+    {
+        return $this->render('page/services.html.twig', []);
+    }
+
+    #[Route('/product', name: 'product')]
+    public function product(): Response
+    {
+        return $this->render('page/product.html.twig', []);
+    }
 }
